95294a8e78d8ef1351fc5b55cff3dcd0218cefa2
mileti <liku0495@gmail.com>
2022-02-26 23:14:41 +0900

    object勉強1

--- object/device.js
+++ object/device.js
@@ -7,11 +7,37 @@ class Device{
 
     confirm() {
         console.log('名前: '+ this.name + ', 現在のエネルギー: ' + this.energy);
-        console.log(`名前: ${this.name} , 現在のエネルギー:  ${this.energy}`);
+        // console.log(`名前: ${this.name} , 現在のエネルギー:  ${this.energy}`);
     }
+
+    move(){
+        if (this.energy > 0 ){
+            this.energy = this.energy - 1;
+            console.log('動作しました。残りエネルギー：' + this.energy);
+        }else{
+            console.log('エネルギーが足りなかったので、動作しませんでした。');
+        }
+    }
+
+
+
 }
 
+// const device = new Device();
+// device.name = 'デバイス1';
+// device.energy = 10;
+// device.confirm();
+
+
 const device = new Device();
-device.name = 'デバイス1';
-device.energy = 10;
+device.name = 'デバイス2';
+device.energy = 3;
+device.confirm();
+
+
+for(let i=1 ; i <= 4 ; i++){
+    device.move();
+}
+
+
 device.confirm();
